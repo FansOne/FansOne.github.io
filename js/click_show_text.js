03f3708ba860626bcdbca3d10eb5d0137d3fc653
FansOne <869262562@qq.com>
2021-03-25 15:50:12 +0800

    Site updated: 2021-03-25 15:50:12

--- js/click_show_text.js
+++ js/click_show_text.js
@@ -2,7 +2,7 @@ var a_idx = 0;
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
         var a = new Array
-        ("富强", "民主", "SM", "I❤U", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
+        ("富强", "民主", "和谐", "文明", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
         var $i = $("<span/>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
